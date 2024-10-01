45be6df9f46054506e69cfdb7ad5a8ac9884ab6b
isaiah <isaiahmartin847@gmail.com>
2024-10-01 15:24:12 -0600

    added the styling to the filter

--- src/app/components/filter/filterForm.tsx
+++ src/app/components/filter/filterForm.tsx
@@ -17,11 +17,26 @@ const FilterForm = () => {
     }
 
     return (
-        <div className="border-2 border-slate-500 p-4">
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Title" onChange={(e: any) => {setTitle(e.target.value)}}></input> 
-                <input type="text" placeholder="City" onChange={(e: any) => {setCity(e.target.value)}}></input> 
-                <input type="text" placeholder="ST" onChange={(e: any) => {setState(e.target.value)}}></input>
+        <div className="p-2 border-b-[1px] border-black">
+            <form onSubmit={handleSubmit} className="flex justify-between">
+                
+                <div className="w-[90%] space-x-4 flex items-center">
+                    <label className="text-lg">
+                        Title: 
+                        <input type="text" className="border-[1px] border-black rounded-lg p-1 ml-2" placeholder="Title" onChange={(e: any) => {setTitle(e.target.value)}}></input> 
+                    </label>
+     
+                    <label className="text-lg">
+                        City:
+                        <input type="text" className="border-[1px] border-black rounded-lg p-1 ml-2" placeholder="City" onChange={(e: any) => {setCity(e.target.value)}}></input> 
+                    </label>
+
+                    <label className="text-lg">
+                        State:
+                        <input type="text" className="border-[1px] border-black rounded-lg p-1 ml-2" placeholder="ST" onChange={(e: any) => {setState(e.target.value)}}></input>
+                    </label>
+                </div>
+
                 <button type="submit" className="border-2 bg-blue-500 p-1 rounded-lg">filter</button>
             </form>
         </div>
