41ff7614da6ca3282f4ddcd4edace8b9dccb447b
isaiah <isaiahmartin847@gmail.com>
2024-10-01 18:32:46 -0600

    made the nav bar and filter mobile freindly

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -3,8 +3,6 @@ import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react"
 import FilterForm from "../filter/filterForm"
-import { handleClientScriptLoad } from "next/script"
-
 
 interface Props {
     title: string 
@@ -13,47 +11,55 @@ interface Props {
     filter: boolean
 }
 
-const NavBar = ( { title, linkName, url, filter }: Props) => {
+const NavBar = ({ title, linkName, url, filter }: Props) => {
     const [filterToggle, setFilterToggle] = useState<boolean>(false)
-    // const [filter, setFilter] = useState<boolean>(true)
-
 
     const handleClick = () => {
         setFilterToggle(!filterToggle)
     }
-    
-    
+
     return (
         <div>
             <div className="flex justify-between items-center h-20 px-5 py-2 border-b-2 border-black">
+                {/* Logo Section */}
                 <div className="w-[200px]">
                     <Link href={"/"}>
                         <Image
                             src="https://cdn.prod.website-files.com/665dd714544b997e4b186636/665ea64edb1e20c176359464_unibui_wordmark_orange-p-500.png"
-                            alt="Picture of the author"
+                            alt="Unibui Logo"
                             width={160}
-                            height={500}
+                            height={100}
+                            priority
                         />
                     </Link>
                 </div>
 
-                <div className="w-[200px] flex justify-center items-center ">
+                {/* Title Section - Hidden on mobile */}
+                <div className="w-[200px] justify-center items-center hidden md:flex">
                     <h1 className="text-[30px] font-mono">{title}</h1>    
                 </div>
 
+                {/* Buttons Section */}
                 <div className="w-[200px] flex justify-center space-x-4">
-                    {filter? <button className="border-2 border-orange-400 px-2 py-1 rounded-lg" onClick={handleClick}>Filter</button>  
-                    :
-                    null}
-                    <Link href={url} className="border-2 border-orange-400 px-2 py-1 rounded-lg">{linkName}</Link>
+                    {filter ? (
+                        <button 
+                            className="border-2 border-orange-400 px-2 py-1 rounded-lg" 
+                            onClick={handleClick}
+                        >
+                            Filter
+                        </button>
+                    ) : null}
+                    
+                    <Link href={url} className="border-2 border-orange-400 px-2 py-1 rounded-lg">
+                        {linkName}
+                    </Link>
                 </div>
             </div>
-            {filterToggle?
-              <FilterForm /> 
-            :
-              null}
+
+            {/* Filter Form Toggle */}
+            {filterToggle ? <FilterForm /> : null}
         </div>
     )
 }
 
-export default NavBar 
+export default NavBar
--- src/app/components/filter/filterForm.tsx
+++ src/app/components/filter/filterForm.tsx
@@ -1,10 +1,12 @@
 import { useRouter, useSearchParams } from "next/navigation"
 import { useState, useEffect } from "react"
+import { IoMdClose } from "react-icons/io"
 
 const FilterForm = () => {
     const [title, setTitle] = useState("")
     const [city, setCity] = useState("")
     const [state, setState] = useState("")
+    const [filtered, setFiltered] = useState<boolean>(true)
     const router = useRouter()
     const searchParams = useSearchParams()
 
@@ -12,7 +14,19 @@ const FilterForm = () => {
         setTitle(searchParams.get('title') || "")
         setCity(searchParams.get('city') || "")
         setState(searchParams.get('state') || "")
+
+        if(searchParams.get('title') || searchParams.get('city') || searchParams.get('state')) {
+            setFiltered(true)
+        } else {
+            setFiltered(false)
+        }
+
     }, [searchParams])
+
+    const clearFilter = (e: React.MouseEvent) => {
+        e.preventDefault()  
+        router.push(`/`)
+    }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
@@ -21,35 +35,35 @@ const FilterForm = () => {
 
     return (
         <div className="p-2 border-b-[1px] border-black">
-            <form onSubmit={handleSubmit} className="flex justify-between">
-                <div className="w-[90%] space-x-4 flex items-center">
-                    <label className="text-lg">
+            <form onSubmit={handleSubmit} className="flex flex-col md:flex-row justify-between">
+                <div className="w-full md:w-[90%] space-y-4 md:space-y-0 md:space-x-4 flex flex-col md:flex-row items-start md:items-center">
+                    <label className="text-lg w-full md:w-auto">
                         Title: 
                         <input 
                             type="text" 
-                            className="border-[1px] border-black rounded-lg p-1 ml-2" 
+                            className="border-[1px] border-black rounded-lg p-1 ml-2 w-full md:w-auto" 
                             placeholder="Title" 
                             value={title}
                             onChange={(e) => setTitle(e.target.value)}
                         />
                     </label>
      
-                    <label className="text-lg">
+                    <label className="text-lg w-full md:w-auto">
                         City:
                         <input 
                             type="text" 
-                            className="border-[1px] border-black rounded-lg p-1 ml-2" 
+                            className="border-[1px] border-black rounded-lg p-1 ml-2 w-full md:w-auto" 
                             placeholder="City" 
                             value={city}
                             onChange={(e) => setCity(e.target.value)}
                         />
                     </label>
 
-                    <label className="text-lg">
+                    <label className="text-lg w-full md:w-auto">
                         State:
                         <input 
                             type="text" 
-                            className="border-[1px] border-black rounded-lg p-1 ml-2" 
+                            className="border-[1px] border-black rounded-lg p-1 ml-2 w-full md:w-auto" 
                             placeholder="ST" 
                             value={state}
                             onChange={(e) => setState(e.target.value)}
@@ -57,7 +71,23 @@ const FilterForm = () => {
                     </label>
                 </div>
 
-                <button type="submit" className="border-2 bg-blue-500 p-1 rounded-lg">filter</button>
+                <div className="flex flex-col md:flex-row items-start md:items-center space-y-2 md:space-y-0 md:space-x-2 mt-4 md:mt-0">
+                    {filtered && (
+                        <button 
+                            onClick={clearFilter} 
+                            className="border-2 border-red-400 flex items-center justify-center p-1 rounded-lg w-full md:w-auto"
+                        >
+                            Clear filter<IoMdClose className="ml-1"/>
+                        </button>
+                    )}
+                    
+                    <button 
+                        type="submit" 
+                        className="border-2 border-blue-500 p-1 rounded-lg w-full md:w-auto"
+                    >
+                        Filter
+                    </button>    
+                </div>
             </form>
         </div>
     )
